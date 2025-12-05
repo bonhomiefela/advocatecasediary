20dc6f9aeb3ff73963e3fd4cce11182502249e35
Bonhomie Fela <87616111+bonhomiefela@users.noreply.github.com>
2025-12-06 01:12:59 +0530

    Implement MainActivity with WebView functionality

---
+++ app/src/main/java/com/bonhomiefela/advocatecasediary/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.bonhomiefela.advocatecasediary
+
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private val startUrl = "https://advocatecasediary.netlify.app/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                // Let the WebView handle the URL
+                return false
+            }
+        }
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(startUrl)
+        } else {
+            webView.restoreState(savedInstanceState)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (::webView.isInitialized) {
+            webView.saveState(outState)
+        }
+    }
+}
